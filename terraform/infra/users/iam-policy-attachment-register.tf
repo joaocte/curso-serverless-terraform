8e583011443a33211d0948452b9ad8773e46351b
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-10 08:11:54 -0300

    first commit

---
+++ terraform/infra/users/iam-policy-attachment-register.tf
@@ -0,0 +1,6 @@
+resource "aws_iam_policy_attachment" "register_policy_attachment" {
+  
+  name       = "${var.environment}-register_policy_attachment"
+  roles      = ["${aws_iam_role.register_iam_role.name}"]
+  policy_arn = "${aws_iam_policy.register_policy.arn}"
+}
---
+++ terraform/infra/users/ssm-paramter-name-table.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "dynamodb-users-table" {
+
+    name = "${var.environment}-dynamodb-users-table"
+    type = "String"
+    value = "${aws_dynamodb_table.users.name}"
+  
+}
