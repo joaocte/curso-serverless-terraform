161f28ef8210f590ae34ddb204ea2a4203e232e6
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-10 11:33:34 -0300

    teste commit

--- terraform/infra/users/iam-policy-attachment-register.tf
+++ terraform/infra/users/iam-policy-attachment-register.tf
@@ -4,3 +4,8 @@ resource "aws_iam_policy_attachment" "register_policy_attachment" {
   roles      = ["${aws_iam_role.register_iam_role.name}"]
   policy_arn = "${aws_iam_policy.register_policy.arn}"
 }
+
+
+
+
+
