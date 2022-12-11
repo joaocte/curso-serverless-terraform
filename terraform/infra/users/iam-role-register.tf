c01db2f79b5481633c8b5e0596de4b1eeae34be6
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-11 09:26:10 -0300

    Create a log policy

--- terraform/infra/users/iam-role-register.tf
+++ terraform/infra/users/iam-role-register.tf
@@ -16,7 +16,6 @@ resource "aws_iam_role" "register_iam_role" {
     ]
   })
 }
-
 resource "aws_ssm_parameter" "register_iam_role" {
   name  = "${var.environment}-register-iam-role"
   type  = "String"
