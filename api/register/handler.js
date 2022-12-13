c8167685ab074ee4df3bf9df91de582fa91302c0
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-13 07:08:31 -0300

    register api

--- api/register/handler.js
+++ api/register/handler.js
@@ -1,15 +1,29 @@
-'use strict';
+const AWS = require('aws-sdk')
+const bcrypt = require('bcryptjs')
+const { v4: uuidv4 } = require('uuid')
 
-module.exports.register = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: process.env.DYNAMODB_USERS
-      }
-    ),
-  };
+AWS.config.update({
+  region: process.env.AWS_Region
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  // caso seja regioes distintas entre a tabela e a lambda, devemos usar a região da tabela
+})
+const documentClient = new AWS.DynamoDB.DocumentClient()
+
+module.exports.register = async event => {
+  const body = JSON.parse(event.body)
+
+  await documentClient.put({
+    TableName: process.env.DYNAMODB_USERS,
+    Item: {
+      id: uuidv4(),
+      name: body.name,
+      email: body.email,
+      password: bcrypt.hashSync(body.password, 10)
+    }
+  }).promise()
+
+  return{
+    statusCode: 201,
+    body: JSON.stringify({message: 'Usuário Inserido com sucesso!'})
+  }
+}
