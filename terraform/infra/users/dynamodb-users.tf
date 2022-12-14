533772a9fc62276f0e4246273985a91870c5f997
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-14 05:49:34 -0300

    criando policy  de login

--- terraform/infra/users/dynamodb-users.tf
+++ terraform/infra/users/dynamodb-users.tf
@@ -5,6 +5,17 @@ resource "aws_dynamodb_table" "users" {
         name = "id"
         type = "S"
     }
+    attribute{
+        name = "email"
+        type = "S"
+    }
+    global_secondary_index {
+    name               = "${var.environment}-email-gsi"
+    hash_key           = "email"
+    write_capacity     = "${var.write_capacity}"
+    read_capacity      = "${var.write_capacity}"
+    projection_type    = "ALL"
+  }
     write_capacity = "${var.write_capacity}"
     read_capacity =  "${var.read_capacity}"  
 }
--- terraform/infra/users/ssm-paramter-name-table.tf
+++ terraform/infra/users/ssm-paramter-name-table.tf
@@ -2,6 +2,14 @@ resource "aws_ssm_parameter" "dynamodb-users-table" {
 
     name = "${var.environment}-dynamodb-users-table"
     type = "String"
-    value = "${aws_dynamodb_table.users.name}"
+    value = "${aws_dynamodb_table.users.name}" 
+}
+
+resource "aws_ssm_parameter" "email-gsi" {
+
+    name = "${var.environment}-email-gsi"
+    type = "String"
+    value = "${var.environment}-email-gsi"
   
 }
+
