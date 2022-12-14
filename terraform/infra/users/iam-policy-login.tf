414fff1db58ecd79520fb0ef0dfe2272b72610d1
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-14 06:54:30 -0300

    Criado template de policy

--- terraform/infra/users/iam-policy-login.tf
+++ terraform/infra/users/iam-policy-login.tf
@@ -1,25 +1,8 @@
 resource "aws_iam_policy" "login_policy" {
 
   name = "${var.environment}-login-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "dynamodb:Query"
-        ]
-        Effect   = "Allow"
-        Resource = "${aws_dynamodb_table.users.arn}/index/${var.environment}-email-gsi"
-      },
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Effect   = "Allow",
-        Resource = "*"
-      },
-    ]
+  policy = templatefile("${path.module}/templates/dynamodb-policy.tpl", {
+    action   = "dynamodb:Query",
+    resource = "${aws_dynamodb_table.users.arn}/index/${var.environment}-email-gsi"
   })
 }
