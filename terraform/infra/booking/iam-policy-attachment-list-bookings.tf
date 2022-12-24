bfc8dd460f391c6fbe4cc2c8dedf9b9271e7fa79
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-24 07:33:22 -0300

    create role and policy to list bookings

---
+++ terraform/infra/booking/iam-policy-attachment-list-bookings.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_policy_attachment" "list_bookings_policy_attachment" {
+  
+  name       = "${var.environment}-list_bookings_policy_attachment"
+  roles      = ["${aws_iam_role.list_bookings_iam_role.name}"]
+  policy_arn = "${aws_iam_policy.list_bookings_policy.arn}"
+}
+
+
+
+
+
