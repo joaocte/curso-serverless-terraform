1c6359f740720a02d5e406201aa9aa4f31c53790
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-24 08:50:23 -0300

    create infra to stream_arn

--- terraform/infra/booking/ssm-paramter-name-table.tf
+++ terraform/infra/booking/ssm-paramter-name-table.tf
@@ -1,6 +1,11 @@
 resource "aws_ssm_parameter" "dynamodb-bookings-table" {
-
     name = "${var.environment}-dynamodb-bookings-table"
     type = "String"
     value = "${aws_dynamodb_table.bookings.name}" 
+}
+resource "aws_ssm_parameter" "dynamodb-bookings-stream" {
+
+    name = "${var.environment}-dynamodb-bookings-stream"
+    type = "String"
+    value = "${aws_dynamodb_table.bookings.stream_arn}" 
 }
